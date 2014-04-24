a9083812976f56f3ed5de3f84cd69ab8e4d1276a
Agregg <agregg@synergydatasystems.com>
2014-04-24 10:07:26 -0400

    adding missing files

---
+++ SmashTO/Migrations/201404241319420_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace SmashTO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Players",
+                c => new
+                    {
+                        PlayerId = c.Int(nullable: false, identity: true),
+                        PlayerName = c.String(nullable: false),
+                        Rating = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PlayerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Players");
+        }
+    }
+}
---
+++ SmashTO/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace SmashTO.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SmashTO.Models.SwissBracketContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(SmashTO.Models.SwissBracketContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
