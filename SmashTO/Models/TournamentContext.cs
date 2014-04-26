e69f6cd9b72fbd78d6bdea3d28590d9d2f184572
Agregg <agregg@synergydatasystems.com>
2014-04-26 14:45:04 -0400

    adding tournamenContext file

---
+++ SmashTO/Models/TournamentContext.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace SmashTO.Models
+{
+    public class TournamentContext : DbContext
+    {
+        public TournamentContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<PlayerModel> Players { get; set; }
+        public DbSet<SwissBracket> SwissBrackets { get; set; }
+        public DbSet<SwissRound> SwissRounds { get; set; }
+        public DbSet<SwissMatch> SwissMatches { get; set; }
+    }
+}
